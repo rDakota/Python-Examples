b64c2e126452ca567fa8e0ce4e64c410dcde54a6
rDakota <rowandakota@gmail.com>
2022-06-01 13:00:36 -0400

    update set.py

--- set.py
+++ set.py
@@ -1,6 +1,7 @@
 class Solution:
     def singleNumber(self, nums: List[int]) -> int:
         # LeetCode solution for single number problem
+        # Set is like HashSet in java
         seen = set()
         for n in nums:
             if n in seen:
