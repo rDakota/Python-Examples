bec5b6edffecbbccc6f2f711609a81c6a1290982
rDakota <rowandakota@gmail.com>
2022-06-01 13:46:13 -0400

    update LargestPerimeterTri.py

--- LeetCode/LargestPerimeterTri.py
+++ LeetCode/LargestPerimeterTri.py
@@ -1,6 +1,7 @@
 class Solution:
     def largestPerimeter(self, A: List[int]) -> int:
         A.sort()        
+        # valid triange == c < a + b
         # decrementing range from last index to but not reaching 2 by -1
         for i in range( (len(A) - 1) , 1 , -1):
             if A[i] < A[i - 1] + A[i - 2]:
