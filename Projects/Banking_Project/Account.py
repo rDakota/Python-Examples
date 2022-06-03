0af6eef8da5989c8709e6b88882ff19b1b8578c6
rDakota <rowandakota@gmail.com>
2022-06-03 11:02:08 -0400

    Create Account.py

--- Projects/Banking_Project/Account.py
+++ Projects/Banking_Project/Account.py
@@ -1,3 +1,13 @@
 import random
 
 class Account: 
+    def __init__(self, id , balance = 0, annualInterestRate = 0.06):
+        self.id = id
+        self.balance = balance
+        self.annualInterestRate = annualInterestRate
+
+    def getId(self):
+        return self.id
+
+    def get_balance(self):
+        return self.balance
